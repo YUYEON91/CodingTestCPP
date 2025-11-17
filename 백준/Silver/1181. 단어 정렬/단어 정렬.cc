a4a5c6f02fff4f5e1504100d32866c35c630595d
YUYEONY <cybott2002@gmail.com>
2025-11-17 10:23:16 +0900

    [Silver V] Title: 단어 정렬, Time: 584 ms, Memory: 4108 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int Ps(string a, string b)
+{
+    if(a.length() == b.length())
+    {
+        return a < b;
+    }
+    else
+    {
+        return a.length() < b.length();
+    }
+}
+
+string word[20000];
+
+int main()
+{
+    int N;
+    
+    cin >> N;
+    for(int i = 0; i < N; i++)
+    {
+        cin >> word[i];
+    }
+    
+    sort(word, word + N, Ps);
+    
+    for(int i = 0; i < N; i++)
+    {
+        if(word[i] == word[i -1])
+           {
+              continue;
+           }
+            cout << word[i] << endl;
+    }
+    
+    return 0;
+}
+
+
